657582e6b41f504794efbc33945061b670473415
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-10-06 19:34:30 +0000

    wireguard: 0.0.20180904 -> 0.0.20181006

--- pkgs/tools/networking/wireguard-tools/default.nix
+++ pkgs/tools/networking/wireguard-tools/default.nix
@@ -4,11 +4,11 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "wireguard-tools-${version}";
-  version = "0.0.20180904";
+  version = "0.0.20181006";
 
   src = fetchzip {
     url = "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-${version}.tar.xz";
-    sha256 = "0slf6lchzhb2san1hsqnny1sdmxq8rzqwz355f33zsgaj6qh1gmk";
+    sha256 = "0283slpyqb8d4cx9wyahqmqi4lwhzamc4i2710zid5y71vjgsgw3";
   };
 
   sourceRoot = "source/src/tools";
