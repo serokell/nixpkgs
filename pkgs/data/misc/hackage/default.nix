6ba8bb0f68e0987546df717fad28b4110e6ee4cb
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-11-13 14:59:18 +0100

    Bump all-cabal-hashes

--- pkgs/data/misc/hackage/default.nix
+++ pkgs/data/misc/hackage/default.nix
@@ -1,6 +1,6 @@
 { fetchurl }:
 
 fetchurl {
-  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/e38571f33da029cee6673ed1e0cf155b10dc9dfb.tar.gz";
-  sha256 = "1dzqwzfqhjaw2j9j37clm1xvczpn7j0ajrlpzgjrjwd916cjs1ch";
+  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/2a8760e4bcbc850c966cb4caaf212b34e390e84d.tar.gz";
+  sha256 = "047fa8vyw79adim6izk4y035z53dr9chnrambp7bfswbqz78jm8i";
 }
