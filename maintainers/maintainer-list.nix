eca73ce3fc8d2f922c958c25fa4a70f5864ed395
Christian Höppner <christian.hoppner@serokell.io>
2019-11-25 16:23:59 +0000

    vault-bin: 1.1.3 -> 1.3.0

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4316,6 +4316,12 @@
     githubId = 16974598;
     name = "Mike Playle";
   };
+  mkaito = {
+    email = "chris@mkaito.net";
+    github = "mkaito";
+    githubId = 20434;
+    name = "Christian Höppner";
+  };
   mkazulak = {
     email = "kazulakm@gmail.com";
     github = "mulderr";
