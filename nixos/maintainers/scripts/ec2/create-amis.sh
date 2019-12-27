31f760049c8a94430f8cd3ab34fd6d67b7018dff
Christian Höppner <christian.hoppner@serokell.io>
2019-12-27 20:12:26 +0000

    [OPS-907] Modify create-amis.sh and amazon-image.nix for Serokell

--- nixos/maintainers/scripts/ec2/create-amis.sh
+++ nixos/maintainers/scripts/ec2/create-amis.sh
@@ -16,14 +16,9 @@ set -euo pipefail
 # configuration
 state_dir=$HOME/amis/ec2-images
 home_region=eu-west-1
-bucket=nixos-amis
+bucket=serokell-nixos-amis
 
-regions=(eu-west-1 eu-west-2 eu-west-3 eu-central-1
-         us-east-1 us-east-2 us-west-1 us-west-2
-         ca-central-1
-         ap-southeast-1 ap-southeast-2 ap-northeast-1 ap-northeast-2
-         ap-south-1 ap-east-1
-         sa-east-1)
+regions=(eu-west-1 eu-west-2 eu-west-3 eu-central-1)
 
 log() {
     echo "$@" >&2
