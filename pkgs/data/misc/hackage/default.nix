952430bc48523c56bc9763061dba34a23c9d27f4
Christian Höppner <christian.hoppner@serokell.io>
2019-12-29 15:32:06 +0000

    Bump all-cabal-hashes

--- pkgs/data/misc/hackage/default.nix
+++ pkgs/data/misc/hackage/default.nix
@@ -1,6 +1,6 @@
 { fetchurl }:
 
 fetchurl {
-  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/93bcfb09798da885d29304fa4dab1e234e3b728e.tar.gz";
-  sha256 = "1mv5kxqldakapzbmch2b88mynng268njq3dxbkmyzli8fwnllra2";
+  url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/691589bd5067468b3b181cb651c8e88b89b7a8ad.tar.gz";
+  sha256 = "1r1bwla563w0inz8dd2n7qi1h96zqa8h55a9v9aw5k62h6sqk28p";
 }
