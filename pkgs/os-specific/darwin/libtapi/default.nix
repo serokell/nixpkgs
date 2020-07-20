f2b4b5037466cb1632760957ae244a6d82a8d60c
Matthew Bauer <mjbauer95@gmail.com>
2020-07-20 15:48:11 -0500

    darwin.libtapi: use newer version for cross builds

--- pkgs/os-specific/darwin/libtapi/default.nix
+++ pkgs/os-specific/darwin/libtapi/default.nix
@@ -1,15 +1,25 @@
 { lib, stdenv, fetchFromGitHub, cmake, python3, clang_6 }:
 
 stdenv.mkDerivation {
-  name = "libtapi-1000.10.8";
-  src = fetchFromGitHub {
-    owner = "tpoechtrager";
-    repo = "apple-libtapi";
-    rev = "cd9885b97fdff92cc41e886bba4a404c42fdf71b";
-    sha256 = "1a19h39a48agvnmal99n9j1fjadiqwib7hfzmn342wmgh9z3vk0g";
-  };
+  name = "libtapi";
+
+  src =
+    if stdenv.hostPlatform != stdenv.targetPlatform then
+      fetchFromGitHub {
+        owner = "tpoechtrager";
+        repo = "apple-libtapi";
+        rev = "3cb307764cc5f1856c8a23bbdf3eb49dfc6bea48";
+        sha256 = "1zb10p6xkls8x7wsdwgy9c0v16z97rfkgidii9ffq5rfczgvrhjh";
+      }
+    else fetchFromGitHub {
+      owner = "tpoechtrager";
+      repo = "apple-libtapi";
+      rev = "cd9885b97fdff92cc41e886bba4a404c42fdf71b";
+      sha256 = "1a19h39a48agvnmal99n9j1fjadiqwib7hfzmn342wmgh9z3vk0g";
+    };
 
   nativeBuildInputs = [ cmake python3 ];
+
   buildInputs = [ clang_6.cc ];
 
   preConfigure = ''
@@ -20,7 +30,7 @@ stdenv.mkDerivation {
 
   buildFlags = "libtapi";
 
-  installTarget = "install-libtapi";
+  installTargets = [ "install-libtapi" "install-tapi-headers"];
 
   meta = with lib; {
     license = licenses.apsl20;
