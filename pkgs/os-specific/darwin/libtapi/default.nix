199a79b903137abba50c2f8ee8ec8b07cbb94d24
Andrew Childs <lorne@cons.org.nz>
2020-07-26 17:27:15 +0900

    libtapi: fix build, install phase and install name
    installTarget looks like a typo for installTargets. This causes a lot
    of llvm and clang to be built and installed.
    
    Clang is not intended to be an external dependency. The source bundle
    includes llvm and clang. Adding include paths and building clangBasic
    first is sufficient to use the internal clang components.

--- pkgs/os-specific/darwin/libtapi/default.nix
+++ pkgs/os-specific/darwin/libtapi/default.nix
@@ -2,28 +2,45 @@
 
 stdenv.mkDerivation {
   name = "libtapi";
-  src = fetchFromGitHub {
-    owner = "tpoechtrager";
-    repo = "apple-libtapi";
-    rev = "e56673694db395e25b31808b4fbb9a7005e6875f";
-    sha256 = "1lnl1af9sszp9wxfk0wljrpdmwcx83j0w5c0y4qw4pqrdkdgwks7";
-  };
+
+  src =
+    if stdenv.hostPlatform != stdenv.targetPlatform then
+      fetchFromGitHub {
+        owner = "tpoechtrager";
+        repo = "apple-libtapi";
+        rev = "3cb307764cc5f1856c8a23bbdf3eb49dfc6bea48";
+        sha256 = "1zb10p6xkls8x7wsdwgy9c0v16z97rfkgidii9ffq5rfczgvrhjh";
+      }
+    else fetchFromGitHub {
+      owner = "tpoechtrager";
+      repo = "apple-libtapi";
+      rev = "cd9885b97fdff92cc41e886bba4a404c42fdf71b";
+      sha256 = "1a19h39a48agvnmal99n9j1fjadiqwib7hfzmn342wmgh9z3vk0g";
+    };
+
+  sourceRoot = "source/src/llvm";
 
   nativeBuildInputs = [ cmake python ];
 
-  preConfigure = ''
-    cd src/apple-llvm/src
-  '';
+  buildInputs = [ clang_6.cc ];
 
   cmakeFlags = [ "-DLLVM_INCLUDE_TESTS=OFF" ];
 
-  buildFlags = "libtapi";
+  # fixes: fatal error: 'clang/Basic/Diagnostic.h' file not found
+  # adapted from upstream
+  # https://github.com/tpoechtrager/apple-libtapi/blob/3cb307764cc5f1856c8a23bbdf3eb49dfc6bea48/build.sh#L58-L60
+  preConfigure = ''
+    INCLUDE_FIX="-I $PWD/projects/clang/include"
+    INCLUDE_FIX+=" -I $PWD/build/projects/clang/include"
+    cmakeFlagsArray+=(-DCMAKE_CXX_FLAGS="$INCLUDE_FIX")
+  '';
 
-  installTarget = "install-libtapi";
+  buildFlags = [ "clangBasic" "libtapi" ];
+
+  installTargets = [ "install-libtapi" "install-tapi-headers" ];
 
   meta = with lib; {
     license = licenses.apsl20;
     maintainers = with maintainers; [ matthewbauer ];
   };
-
 }
