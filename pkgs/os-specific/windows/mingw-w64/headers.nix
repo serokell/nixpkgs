1ccf528dbe4f27037c410829c8127a4e56d10b44
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2020-09-14 14:54:17 +0300

    Revert "mingw-w64: 5.0.4 -> 6.0.0"
    This reverts commit 8b89e0494c5afc511df5463a413c340b43f73ca9.

--- pkgs/os-specific/windows/mingw-w64/headers.nix
+++ pkgs/os-specific/windows/mingw-w64/headers.nix
@@ -4,6 +4,8 @@ stdenvNoCC.mkDerivation {
   name = "${mingw_w64.name}-headers";
   inherit (mingw_w64) src meta;
 
+  patches = [ ./osvi.patch ];
+
   preConfigure = ''
     cd mingw-w64-headers
   '';
