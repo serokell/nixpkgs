8cc92e59ef11b1dd372dadb8fd8cd83f6eb10e62
Artemis Tosini <me@artem.ist>
2021-01-26 18:38:37 +0000

    sudo: 1.9.5p1 → 1.9.5p2

--- pkgs/tools/security/sudo/default.nix
+++ pkgs/tools/security/sudo/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname = "sudo";
-  version = "1.9.4p2";
+  version = "1.9.5p2";
 
   src = fetchurl {
     url = "https://www.sudo.ws/dist/${pname}-${version}.tar.gz";
-    sha256 = "0r0g8z289ipw0zpkhmm33cpfm42j01jds2q1wilhh3flg7xg2jn3";
+    sha256 = "0y093z4f3822rc88g9asdch12nljdamp817vjxk04mca7ks2x7jk";
   };
 
   prePatch = ''
