3de2448b09c76ab56baa944f9b854780ccff60ab
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2021-08-11 14:01:46 +0300

    danger-gitlab: init at 8.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3886,6 +3886,8 @@ in
 
   daemonize = callPackage ../tools/system/daemonize { };
 
+  danger-gitlab = callPackage ../applications/version-management/danger-gitlab { };
+
   daq = callPackage ../applications/networking/ids/daq { };
 
   dar = callPackage ../tools/backup/dar { };
