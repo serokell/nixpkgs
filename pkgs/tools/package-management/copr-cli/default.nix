6ee8904779bff21f4b43df9b8b7b8d86a469685f
Roman Melnikov <rvembox@gmail.com>
2024-05-31 14:09:18 +0200

    copr-cli: fix build configuration (#71)
    Problem: 'copr-cli' fails with 'ModuleNotFoundError: No module named
    'pkg_resources'' in runtime.
    
    Solution: Add 'setuptools' to 'propagatedBuildInputs'.

--- pkgs/tools/package-management/copr-cli/default.nix
+++ pkgs/tools/package-management/copr-cli/default.nix
@@ -1,7 +1,7 @@
-{ fetchFromGitHub, python39Packages }:
+{ fetchFromGitHub, python312Packages }:
 
 let
-  inherit (python39Packages) buildPythonApplication;
+  inherit (python312Packages) buildPythonApplication;
   common-src = fetchFromGitHub {
     owner = "fedora-copr";
     repo = "copr";
@@ -9,15 +9,15 @@ let
     sha256 = "1qhn1l4ikkf6z391q1n68m9c50h1wvxya4w25pnzryklqz9xfra4";
   };
   python-copr = buildPythonApplication {
-    propagatedBuildInputs = with python39Packages;
+    propagatedBuildInputs = with python312Packages;
       [ requests-toolbelt requests marshmallow six munch ];
     src = "${common-src}/python";
     name = "copr";
     version = "1.112";
   };
 in buildPythonApplication rec {
-  propagatedBuildInputs = with python39Packages;
-    [ requests humanize jinja2 simplejson python-copr ];
+  propagatedBuildInputs = with python312Packages;
+    [ requests humanize jinja2 simplejson python-copr setuptools ];
   src = "${common-src}/cli";
   name = "copr-cli";
   version = "1.95-1";
