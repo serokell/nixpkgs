d448b7151ef6dc2e22393429606986e38b7546f4
Roman Melnikov <roman.melnikov@serokell.io>
2024-12-02 15:32:02 +0100

    nginx: zlib-ng patch

--- pkgs/servers/http/nginx/stable.nix
+++ pkgs/servers/http/nginx/stable.nix
@@ -1,6 +1,12 @@
-{ callPackage, ... } @ args:
+{ callPackage, fetchurl, ... } @ args:
 
 callPackage ./generic.nix args {
   version = "1.26.2";
   hash = "sha256-Yn/ghiCbuoCihToK3Z2VjX673/oahGeleEyaa08D1zg=";
+  extraPatches = [
+    # A part of https://github.com/zlib-ng/patches/tree/master/nginx#1262-zlib-ngpatch
+    # that increases the size of pre-allocated memory to fit with zlib-ng requirements
+    # to avoid spamming logs with 'gzip filter failed to use preallocated memory' error
+    ./nix-zlib-ng-gzip-buffer-1.26.2.patch
+  ];
 }
